485c7912228dc2ab77e0c8f0660784e2b3220b15
SachinWankhade67369 <sonofrj19@gmail.com>
2021-03-17 20:24:09 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/shop_api_integration/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shop_api_integration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shop_api_integration/views/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop_Api',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
